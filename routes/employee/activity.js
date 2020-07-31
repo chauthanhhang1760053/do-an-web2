022a2626139fb9133560385864e396d79c9d4f06
Đức Hải <57825881+duchai157012@users.noreply.github.com>
2020-07-31 18:31:15 +0700

    Update router for /activity

--- routes/employee/activity.js
+++ routes/employee/activity.js
@@ -1,9 +1,16 @@
-module.exports = function index(req,res){
+const { Router } = require('express');
+const Employee = require('../../services/employee');
+const asyncHandler = require('express-async-handler');
+
+const router = new Router();
+
+router.get('/', function index(req,res){
     //Nếu nhân viên chưa đăng nhập thì redirect về trang đăng nhập cho nhân viên
     if(!req.currentUser) {
         res.redirect('employee');
     }    
-    const employee=require('../../services/employee');
     res.render('employee/activity', { user: req.currentUser });
-    
-};
+});
+
+
+module.exports = router;
