40cc264782076910cfbd08f9cb128817adb2607c
1760086 <lntkien.17ck1@gmail.com>
2020-08-07 02:54:09 +0700

    new emp

---
+++ services/activity.js
@@ -0,0 +1,7 @@
+module.exports = function (req,res){
+    if(!req.currentEmployee) {
+        res.redirect('/employee');
+    }    
+    const employee=req.currentEmployee;
+    res.render('employee/activity', { employee: employee });    
+};
