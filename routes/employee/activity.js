faf2787d5c0cf0692bfe477873b70561312e0266
1760086 <lntkien.17ck1@gmail.com>
2020-08-07 02:50:15 +0700

    dl acti and emp

--- routes/employee/activity.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req,res){
-    if(!req.currentEmployee) {
-        res.redirect('/employee');
-    }    
-    const employee=req.currentEmployee;
-    res.render('employee/activity', { employee: employee });    
-};
