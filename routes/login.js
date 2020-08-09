2c0a31e2395e1bbdf74d46df4ad91e202162bba2
1760086 <lntkien.17ck1@gmail.com>
2020-08-09 07:25:40 +0700

    1760086 login.js

---
+++ routes/login.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const User = require('../services/users');
+const asyncHandler = require('express-async-handler');
+const router = new Router();
+
+router.get('/', asyncHandler(async function getlogin(req, res) {
+    res.render('login'); 
+}));  
+router.post('/', asyncHandler(async function postLogin(req, res) {
+    const user = await User.findUserByEmail(req.body.email)
+    if (!user || !User.verifyPassword(req.body.password, user.password)) {
+        return res .render('login')
+    }   
+    req.session.userId = user.id
+    res.redirect('user');
+})); 
+
+module.exports = router;
+
