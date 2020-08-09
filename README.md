32b108980b2dd28d9c50d07ab8d547a2ec0daba4
1760086 <lntkien.17ck1@gmail.com>
2020-08-10 03:57:31 +0700

    update server

--- README.md
+++
@@ -1,6 +0,0 @@
-Đồ Án Web 2
-Danh sách thành viên nhóm: 
-1760053 - Châu Thanh Hằng
-1760086 - Lê Nguyễn Trung Kiên
-1760137 - Nguyễn Ngọc Yến Nhi
-1760299 - Nguyễn Ngọc Đức Hải
--- server.js
+++ server.js
@@ -1,48 +1,62 @@
-const express =require('express');
-const bodyParser = require('body-parser');
-const cookieSession=require('cookie-session');
-const db=require('./services/db');
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 
-const port=process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
+const express = require('express');
 const app = express();
-app.set('views','./views');
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false}));
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const cors = require('cors');
+const db = require('./services/db');
 
-app.use(cookieSession({
-    name: 'session',
-    keys: ['12345'],
-    maxAge: 24*60*60*1000, 
-}));
+app.use(
+	cookieSession({
+		name: 'nkhh-bank',
+		keys: [ 'nkhh' ],
+		maxAge: 24 * 60 * 60 * 1000 //24h
+	})
+);
 
-//Midldeware
-app.use(require('./middlewares/auth'));
-app.use(require('./middlewares/employee'));
+app.use(require('./middleware/authentication').localCurrentUser);
 
-//Routes
 app.use(express.static('public'));
-app.get('/',require('./routes/index')); 
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
 
-//Employee
-app.use('/employee',require('./routes/employee/employee'));
-app.use('/activity',require('./routes/employee/activity'));
-app.use('/xacthuc',require('./routes/employee/xacthuc'));
-app.use('/naptien',require('./routes/employee/naptien'));
-app.use('/employee/logout', require('./routes/employee/logout'));
-app.use('/listUsers', require('./routes/employee/listUsers'));
+app.use(cors());
+app.use('/api', require('./api'));
 
-//User
-app.use('/login',require('./routes/login'));
-app.use('/register',require('./routes/register'));
-app.use('/user',require('./routes/user/index'));
-app.use('/active',require('./routes/user/active'));
-app.get('/logout' ,require('./routes/logout'));
+//set layout
+// app.set('socketio', io);
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-db.sync().then(function(){
-    app.listen(port);
-    console.log(`Server is listening on ${port}`);
-}).catch(function(err){
-    console.error(err);
-});
+//ROUTER
+app.use('/', require('./routes/index'));
+app.use('/login', require('./routes/login'));
+app.use('/register', require('./routes/register'));
+app.use('/active', require('./routes/active'));
+app.use('/forget-password', require('./routes/forgetPassword'));
+app.use('/profile', require('./routes/profile'));
+app.use('/transfer', require('./routes/transfer'));
+app.use('/withdraw', require('./routes/withdraw'));
+app.use('/verify-card', require('./routes/verify-card'));
+app.use('/list-user', require('./routes/list-user'));
+app.use('/history', require('./routes/history'));
+app.use('/history-user', require('./routes/history-user'));
+app.use('/list-account', require('./routes/list-account'));
+app.use('/about', require('./routes/about'));
+app.use('/privacy', require('./routes/privacy'));
+
+db
+	.sync()
+	.then(function() {
+		app.listen(port);
+		console.log(`\nServer is listening on port ${port}\n`);
+	})
+	.catch(function(err) {
+		console.error(err);
+	});
