c1d9f543db48a2440e0ffedaff6767dc3aa53a66
Đức Hải <57825881+duchai157012@users.noreply.github.com>
2020-08-09 12:39:44 +0700

    edit css

--- routes/employee/activity.js
+++ routes/employee/activity.js
@@ -1,8 +1,7 @@
-module.exports = function (req,res){
-    if(!req.currentEmployee) {
+module.exports = function (req, res) {
+    if (!req.currentEmployee) {
         res.redirect('/employee');
-    }    
-    const employee=require('../../services/employee');
-    res.render('employee/activity', { user: req.employee });    
+    }
+    const employee = require('../../services/employee');
+    res.render('employee/activity', { user: req.employee });
 };
- 
