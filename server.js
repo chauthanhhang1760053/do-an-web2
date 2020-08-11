18c02107c603bd8e6e785d43ac4c3ac757ea7f71
Nhi <nnynhi.17ck1@gmail.com>
2020-08-11 15:10:50 +0700

    137-118-3

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
 		keys: [ 'nkhh' ],
 		maxAge: 24 * 60 * 60 * 1000 //24h
 	})
-);
+); 
 
 app.use(require('./middleware/authentication').localCurrentUser);
 
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use(cors());
+app.use(cors()); 
 app.use('/api', require('./api'));
 
 //set layout
